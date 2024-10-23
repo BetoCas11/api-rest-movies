c3c99a9ef8c9b75e2d90654aac6398b035fcc8e7
José Alberto <albertomajalca159@gmail.com>
2024-10-22 18:41:43 -0600

    Estilos hechos, aun falta un filtro en la sección del aside y los iconos

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ class MovieCard extends HTMLElement{
     getItem(){
         return /*html */`
         <figure>
-            <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8PYqGSd8MOm5ce8io4qNSAiSExW.jpg" alt="" width="180" height="280">
+            <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8PYqGSd8MOm5ce8io4qNSAiSExW.jpg" alt="Poster película">
             <figcaption>
-                <h3>Título de la película sssssssssssssss</h3>
+                <h3>Título de la película: ALIEN</h3>
                 <p>Fecha de Lanzamiento</p>
             </figcaption>
         </figure>
@@ -23,17 +23,39 @@ class MovieCard extends HTMLElement{
             <style>
                 figure{
                     margin: 0;
-                    padding-block-start: 10px;
                     block-size: max-content;
-                    inline-size: 180px;
-                    background: green;
-                    display: grid;
-                    place-content: center;
+                    inline-size: 150px;
+                    outline: 1px solid var(--graycontorn);
+                    border-radius: 10px;
+                    overflow: hidden;
+                    background-color: var(--lightblack-color);
+                    transition: scale 300ms ease,
+                                box-shadow 300ms ease;
+                    cursor: pointer;
+                    &:hover{
+                        scale: 1.1;
+                        box-shadow: 0 0 40px var(--textnav-color);
+                    }
                     & img{
-                        border-radius: 20px;
+                        inline-size: 100%;
+                        block-size: 160px;
+                        object-fit: cover;
+                    }
+                    & figcaption{
+                        block-size: 100%;
+                        & h3{
+                            color: color-mix(in srgb, var(--textnav-color) 50%, white);
+                            text-shadow: 0 0 9px white;
+                            font-size: 20px;
+                        }
+                        & p{
+                            color: color-mix(in srgb, var(--textnav-color) 30%, white);
+                            font-size: 15px;
+                        }
                     }
                     & figcaption > *{
                         text-align: center;
+                        
                     }
                 }
 
